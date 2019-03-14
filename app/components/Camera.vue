9a32ade3ebd59cd4d7b00a6f320b5ee8a3c40938
Anthony zink <anthony.zink@outlook.fr>
2019-03-14 10:37:30 +0100

    ajout de la recuperation des coordonnées

--- app/components/Camera.vue
+++ app/components/Camera.vue
@@ -14,6 +14,9 @@ import * as imagepicker from "nativescript-imagepicker";
 
 import { Image } from "tns-core-modules/ui/image";
 
+//avec import ça marche pas
+var geolocation = require("nativescript-geolocation");
+
 export default {
 	data() {
 		return {
@@ -45,23 +48,65 @@ export default {
 			});
 
 		},
+
+		getLocalisation(){
+
+			let localisation = {}
+
+			return new Promise(function(resolve, reject) {
+
+				geolocation.isEnabled().then((isEnabled) => {
+					if (!isEnabled) {
+						geolocation.enableLocationRequest().then(() => {
+							console.log("demande");
+							
+						},(e) => {
+							console.log(e)
+						});
+					}else{
+						let location = geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000}).then(function(loc) {
+								if (loc) {
+									localisation.latitude = loc.latitude;
+									localisation.longitude = loc.longitude
+									resolve(localisation);
+		
+								}
+							}, function(e){
+								console.log("Error: " + e.message);
+						});
+					}
+					console.log('fin');
+				})
+			});
+			
+		},
 		takePicture() {
-			camera.requestPermissions()
-			.then(() => {
-				camera.takePicture({ width: 300, height: 300, keepAspectRatio: true, saveToGallery:false })
-				.then(imageAsset => {
-					let img = new Image();
-					img.src = imageAsset;
-					this.images.push(img);
-					console.log('ive got '+this.images.length+' images now.');
+
+			this.getLocalisation().then(loc => {
+				
+				camera.requestPermissions()
+				.then(() => {
+					camera.takePicture({ width: 300, height: 300, keepAspectRatio: true, saveToGallery:false })
+					.then(imageAsset => {
+						let img = new Image();
+						img.src = imageAsset;
+						img.location = loc;
+						this.images.push(img);
+						console.log(this.images);
+						
+						console.log(`ive got ${this.images.length} images now at location : ${img.location.latitude}, ${img.location.longitude}` );
+					})
+					.catch(e => {
+						console.log('error:', e);
+					});
 				})
 				.catch(e => {
-					console.log('error:', e);
+					console.log('Error requesting permission');
 				});
 			})
-			.catch(e => {
-				console.log('Error requesting permission');
-			});
+			
+			
+
 		}
 	}
 }
